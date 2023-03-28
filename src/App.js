e100b8b66022bce16245c21bdbd4a758008d9e3c
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-03-28 00:30:18 -0500

    further testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-tailwind-portfolio">
       <div className="min-h-screen flex flex-col">
         <Header />
         <div className="grow flex flex-col justify-evenly">
