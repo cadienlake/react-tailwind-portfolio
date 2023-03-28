35220b074d960a81f4080e34c075dd0457c1fdd6
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-03-28 00:54:17 -0500

    Adjust index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
-ReactDOM.render(
-  <BrowserRouter basename="{process.env.PUBLIC_URL}">
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(
+//   <BrowserRouter basename="{process.env.PUBLIC_URL}">
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+ReactDOM.render(<App />, document.getElementById("root"));
