c8d9036ea27cb2ef6632f53a8cfe23df23c7cc51
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-03-28 00:24:15 -0500

    Imported browser router to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
